3f0f6c599bce4ed5ee98b37baf73d8ed8002d6ea
ObjectOne <chameera@objectoneworld.com>
2021-07-23 11:44:27 +0530

    added notification messages

--- src/app/pages/manage-staff/manage-staff.component.ts
+++ src/app/pages/manage-staff/manage-staff.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {SystemEmployeeService} from '../../service/system-employee.service';
-import {Employee} from '../../model/employee';
+import {Component, OnInit} from '@angular/core'
+import {SystemEmployeeService} from '../../service/system-employee.service'
+import {Employee} from '../../model/employee'
+import {NotificationService} from '../../shared/util/notification.service'
 
 @Component({
   selector: 'app-manage-staff',
@@ -9,58 +10,60 @@ import {Employee} from '../../model/employee';
 })
 export class ManageStaffComponent implements OnInit {
 
-  employeeList : Employee[];
-  empFirstName : string;
-  empLastName : string;
-  empMobile : string;
-  empEmail : string;
-  empDesignation : string;
-  empNic : string;
+  employeeList: Employee[]
+  empFirstName: string
+  empLastName: string
+  empMobile: string
+  empEmail: string
+  empDesignation: string
+  empNic: string
 
-  constructor(private systemEmployeeService : SystemEmployeeService) { }
+  constructor(private systemEmployeeService: SystemEmployeeService, private notification: NotificationService) {
+  }
+
   visited = [
     {
 
-        firstname: 'ebfhbfhf',
-        lastname: 'dehyuehfuhfu',
-        designation: 'jknknkn',
-        nicpassport: '554545',
-        tel: '456454545',
-        email:'eygfygyf',
+      firstname: 'ebfhbfhf',
+      lastname: 'dehyuehfuhfu',
+      designation: 'jknknkn',
+      nicpassport: '554545',
+      tel: '456454545',
+      email: 'eygfygyf'
 
 
-      },
-
-
+    }
 
 
   ]
-  pageOfItems: Array<any>;
- onChangePage(pageOfItems: Array<any>) {
-  // update current page of items
-  this.pageOfItems = pageOfItems;
-}
+  pageOfItems: Array<any>
+
+  onChangePage(pageOfItems: Array<any>) {
+    // update current page of items
+    this.pageOfItems = pageOfItems
+  }
 
   ngOnInit() {
-   this._getSystemStaff();
+    this._getSystemStaff()
   }
 
-  _getSystemStaff(){
-    this.systemEmployeeService.getEmployeeList('',0).subscribe((data:Object[])=>{
-      console.log(data);
-      this.employeeList = data['body'].content;
-    },error => {
-      alert("Records Not Found");
-    });
+  _getSystemStaff() {
+    this.systemEmployeeService.getEmployeeList('', 0).subscribe((data: Object[]) => {
+      console.log(data)
+      this.employeeList = data['body'].content
+    }, error => {
+      alert("Records Not Found")
+      this.notification.showError("Records Not Found","")
+    })
   }
 
-  _createNewEmployee(){
-   this.systemEmployeeService.createEmployee(this.empFirstName,this.empLastName,this.empNic,this.empEmail,this.empMobile,this.empDesignation).subscribe((data)=>{
-     console.log(data['success']);
-     if (data['success']){
-       console.log('aaa')
-     }
-   })
+  _createNewEmployee() {
+    this.systemEmployeeService.createEmployee(this.empFirstName, this.empLastName, this.empNic, this.empEmail, this.empMobile, this.empDesignation).subscribe((data) => {
+      console.log(data['success'])
+      if (data['success']) {
+        console.log('aaa')
+      }
+    })
   }
 
 }
