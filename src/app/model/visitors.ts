0fdb986b31e67354450fe704d7d031084791792c
Ramesh97 <hwr@1997>
2021-07-28 22:24:16 +0530

    Version 2021/07/28 Data Binding

---
+++ src/app/model/visitors.ts
@@ -0,0 +1,9 @@
+export class Visitors {
+  visitorId : number;
+  firstName : string;
+  lastName : string;
+  mobile : string;
+  nic : string;
+  email : string;
+  createDate : string;
+}
---
+++ src/app/service/visitors.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VisitorsService {
+
+  private BASE_URL = 'http://198.211.109.205:8082/';
+
+  constructor(private httpClient : HttpClient) { }
+
+  public getAllVisitors(searchText,pageNo){
+    let url = `${this.BASE_URL+'v1/visitor/filter?word='+searchText+'&index='+pageNo+'&size=10'}`;
+    let token = localStorage.getItem('access_token');
+    const headers =
+      new HttpHeaders({
+        'Authorization': 'Bearer '+token,
+        'Content-Type': 'application/json'
+      });
+
+    return this.httpClient.get(url,{headers});
+  }
+}
